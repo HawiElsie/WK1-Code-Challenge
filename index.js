91aaba6bb011a36016303b88c3c1704a08627ec6
Elsie <hawiokatch@gmail.com>
2023-03-22 14:30:08 +0300

    complete assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+// Copyright 2023 hawi
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+<h2>Student Grade Generator</h2>
+
+//mark
+//When mark is between >79 to 100: A
+//When mark is between 60 to 79: B
+//When mark is between 50 to 59: c
+//When mark is between 40 to 49: D
+//When mark is less than 40: E
+
+//let mark = 68
+
